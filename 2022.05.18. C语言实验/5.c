e922b5662f33ec9979ec4bd08d10127813d32673
Huwenqiang Xiaomi Manjaro <49776858+Hu-Wenqiang@users.noreply.github.com>
2022-05-18 09:59:28 +0800

    Updated C

--- 2022.05.18. C语言实验/5.c
+++ 2022.05.18. C语言实验/5.c
@@ -1,30 +1,41 @@
-/*************************************************************************
-    > File Name: 打印钻石.c
-    > Author: 21Q017
-    > Mail: 905505155@qq.com 
-    > Created Time: 2022年05月18日 星期三 09时29分57秒
- ************************************************************************/
+/**
+* @File_Name: 4.c
+* @Author: Wenqiang Hu
+* @Mail: huwenqiang.hwq@protonmail.com
+* @Created_Time: 2022年05月18日 星期三 09时36分15秒
+* @Description: 输出菱形
+**/
 
 #include<stdio.h>
+
 int main(){
-	int row,half,i,j;
+	int row,half;
+
 	printf("输入要打印的行数:");
-	scanf("%d",&row);
+	scanf("%d", &row);
+
 	half = row / 2 + 1;
-	for(i = 1;i <= half; i++){
-		for(j = 1; j <= half -i; j++){
+	for(int i = 1; i <= half; i++)
+    {
+		for(int j = 1; j <= half -i; j++)
+        {
 			printf(" ");
 		}
-		for(j = 1;j <= 2 * i - 1;j++){
+		for(int j = 1;j <= 2 * i - 1;j++)
+        {
 			printf("*");
 		}
 		printf("\n");
 	}
-	for(i = 1;i < half; i++){
-		for(j = 1;j <= i; j++){
+
+	for(int i = 1;i < half; i++)
+    {
+		for(int j = 1; j <= i; j++)
+        {
 			printf(" ");
 		}
-		for(j = 1;j <= 2*(half-i-1) + 1;j++){
+		for(int j = 1; j <= 2*(half-i-1) + 1; j++)
+        {
 			printf("*");
 		}
 		printf("\n");
