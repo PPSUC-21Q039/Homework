6cb75ca151ea9808f8b0090b82482ca711a40498
Huwenqiang Xiaomi Manjaro <49776858+Hu-Wenqiang@users.noreply.github.com>
2022-05-25 10:45:26 +0800

    Updated 3.c

--- 2022.05.18. C语言实验/第二次实验/3.c
+++ 2022.05.18. C语言实验/第二次实验/3.c
@@ -3,7 +3,7 @@
 * @Author: Wenqiang Hu
 * @Mail: huwenqiang.hwq@protonmail.com
 * @Created_Time: Wed May 25 10:01:55 2022
-* @Description: 
+* @Description: 餐饮评价等级统计
 **/
 
 #include <stdio.h>
@@ -13,7 +13,6 @@ int main()
     int valuation[1000] = {0};
     int count = 0;
     int input;
-    int size = 0;
     int grade_1 = 0, \
         grade_2 = 0, \
         grade_3 = 0, \
@@ -32,8 +31,31 @@ int main()
             break;
         }
     }
-    size = count;
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < count; i++)
     {
-        if ()
+        switch (valuation[i])
+        {
+            case 1: 
+                grade_1 ++; break;
+            case 2:
+                grade_2 ++; break;
+            case 3:
+                grade_3 ++; break;
+            case 4:
+                grade_4 ++; break;
+            case 5:
+                grade_5 ++; break;
+        }
+    }
+    
+    printf("n = %d\n", count);
+    printf("Grade(*)      Count(n)      Percent(%)\n");
+    printf("5             %-5d         %-2.0f\n", grade_5, (float)((float)grade_5 * 100 / (float)count));
+    printf("4             %-5d         %-2.0f\n", grade_4, (float)((float)grade_4 * 100 / (float)count));
+    printf("3             %-5d         %-2.0f\n", grade_3, (float)((float)grade_3 * 100 / (float)count));
+    printf("2             %-5d         %-2.0f\n", grade_2, (float)((float)grade_2 * 100 / (float)count));
+    printf("1             %-5d         %-2.0f\n", grade_1, (float)((float)grade_1 * 100 / (float)count));
+
+    return 0;
+}
